57101b29467aa24348129fbc74cd6997fae3c954
nasiruddin01 <nasiruddin.dev@gmail.com>
2021-08-19 09:59:00 +0600

    first-commit

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
+// external emports
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const path = require("path");
+const { cookie } = require("express-validator");
+const cookieParser = require("cookie-parser");
+const loginRouter = require("./router/loginRouter");
+
+// internal import
+const {
+  notFoundHandler,
+  errorHandler,
+} = require("./middlewares/common/errorHandler");
 
 const app = express();
 dotenv.config();
@@ -13,3 +24,29 @@ mongoose
   })
   .then(() => console.log("database connection successful!"))
   .catch((err) => console.log(err));
+
+//   request parsers
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//set view engine
+app.set("view engine", "ejs");
+
+// set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// parse cookies
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+// routing setup
+app.use("/", loginRouter);
+// app.use("/users", usersRouter);
+// app.use("/inbox", inboxRouter);
+// 404 not found handler
+app.use(notFoundHandler);
+// common error handler
+app.use(errorHandler);
+
+app.listen(process.env.PORT, () => {
+  console.log(`app listening to port ${process.env.PORT}`);
+});
